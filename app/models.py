c007abdf8abd85017740dbe400a97d8a4aea83e0
lil-flud <flashflud28@yahoo.com>
2023-01-31 11:54:33 -0600

    First commit, need to figure out why incrementation works on its own but not later.

---
+++ app/models.py
@@ -0,0 +1,62 @@
+from django.db import models
+
+# Create your models here.
+
+
+class TaxiBusiness(models.Model):
+    occupied = models.BooleanField()
+    capacity = models.PositiveIntegerField()
+    current_passengers = models.PositiveIntegerField()
+    fare = models.FloatField()
+    taxi_type = models.TextField()
+    notes = models.TextField()
+    taxi_number = 100
+    amt_taxis = 1
+
+
+def add_taxi(
+    occupied, capacity, current_passengers, fare, taxi_type, taxi_number, notes
+):
+    taxi = TaxiBusiness(
+        occupied=occupied,
+        capacity=capacity,
+        current_passengers=current_passengers,
+        fare=fare,
+        taxi_type=taxi_type,
+        notes=notes,
+    )
+    taxi.save()
+    ret_taxi = taxi
+    ret_taxi.taxi_number = ret_taxi.taxi_number + (taxi.id * 11)
+    ret_taxi.save()
+    return ret_taxi
+
+
+def start_fare(num_pass):
+    unocc_taxis = TaxiBusiness.objects.filter(occupied=False, capacity__gte=num_pass)
+    first_avail_taxi = unocc_taxis[0]
+    first_avail_taxi.current_passengers += num_pass
+    first_avail_taxi.occupied = True
+    first_avail_taxi.save()
+    return first_avail_taxi
+
+
+# def add_taxi(
+#     occupied, capacity, current_passengers, fare, taxi_type, taxi_number, notes
+# ):
+#     taxi = TaxiBusiness(
+#         occupied=occupied,
+#         capacity=capacity,
+#         current_passengers=current_passengers,
+#         fare=fare,
+#         taxi_type=taxi_type,
+#         taxi_number=taxi_number,
+#         notes=notes,
+#     )
+#     taxi.save()
+#     if len(TaxiBusiness.objects.all()) >= 1:
+#         taxi.taxi_number += 11 * len(TaxiBusiness.objects.all())
+#         taxi.save()
+#         return taxi
+#     else:
+#         return taxi
---
+++ app/tests.py
@@ -0,0 +1,120 @@
+from django.test import TestCase
+from app import models
+
+# Create your tests here.
+
+
+class TestTaxi(TestCase):
+    def test_can_create_taxi(self):
+        taxi = models.add_taxi(
+            False,
+            4,
+            0,
+            2.50,
+            "Van",
+            0,
+            "None",
+        )
+
+        self.assertEqual(taxi.id, 1)
+        self.assertFalse(taxi.occupied, False)
+        self.assertEqual(taxi.capacity, 4)
+        self.assertEqual(taxi.current_passengers, 0)
+        self.assertEqual(taxi.fare, 2.50)
+        self.assertEqual(taxi.taxi_type, "Van")
+        self.assertEqual(taxi.taxi_number, 111)
+        self.assertEqual(taxi.notes, "None")
+
+        taxi2 = models.add_taxi(
+            True,
+            2,
+            2,
+            3.50,
+            "Car",
+            0,
+            "Hi",
+        )
+
+        self.assertEqual(taxi2.id, 2)
+        self.assertTrue(taxi2.occupied, True)
+        self.assertEqual(taxi2.capacity, 2)
+        self.assertEqual(taxi2.current_passengers, 2)
+        self.assertEqual(taxi2.fare, 3.50)
+        self.assertEqual(taxi2.taxi_type, "Car")
+        self.assertEqual(taxi2.taxi_number, 122)
+        self.assertEqual(taxi2.notes, "Hi")
+
+        taxi3 = models.add_taxi(
+            True,
+            2,
+            2,
+            3.50,
+            "Car",
+            0,
+            "Hi",
+        )
+
+        self.assertEqual(taxi3.id, 3)
+        self.assertTrue(taxi3.occupied, True)
+        self.assertEqual(taxi3.capacity, 2)
+        self.assertEqual(taxi3.current_passengers, 2)
+        self.assertEqual(taxi3.fare, 3.50)
+        self.assertEqual(taxi3.taxi_type, "Car")
+        self.assertEqual(taxi3.taxi_number, 133)
+        self.assertEqual(taxi3.notes, "Hi")
+
+    def test_start_fare(self):
+        taxis = [
+            {
+                "occupied": False,
+                "capacity": 4,
+                "current_passengers": 0,
+                "fare": 2.50,
+                "taxi_type": "Van",
+                "taxi_number": 0,
+                "notes": "None",
+            },
+            {
+                "occupied": True,
+                "capacity": 3,
+                "current_passengers": 3,
+                "fare": 4.00,
+                "taxi_type": "Van",
+                "taxi_number": 0,
+                "notes": "None",
+            },
+            {
+                "occupied": False,
+                "capacity": 2,
+                "current_passengers": 0,
+                "fare": 1.50,
+                "taxi_type": "car",
+                "taxi_number": 0,
+                "notes": "None",
+            },
+            {
+                "occupied": False,
+                "capacity": 6,
+                "current_passengers": 0,
+                "fare": 5.50,
+                "taxi_type": "Yukon",
+                "taxi_number": 0,
+                "notes": "None",
+            },
+        ]
+
+        for taxi in taxis:
+            models.add_taxi(
+                taxi["occupied"],
+                taxi["capacity"],
+                taxi["current_passengers"],
+                taxi["fare"],
+                taxi["taxi_type"],
+                taxi["taxi_number"],
+                taxi["notes"],
+            )
+
+        taxi = models.start_fare(5)
+        self.assertEqual(taxi.taxi_type, "Yukon")
+        self.assertEqual(taxi.current_passengers, 5)
+        self.assertEqual(taxi.taxi_number, 144)
